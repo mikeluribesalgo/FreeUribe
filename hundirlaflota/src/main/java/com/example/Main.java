9d6fd6ec586248ee5f241976baabad36a5a7ecce
Mikel Uribesalgo Arbulu <mikel.uribesalgo@alumni.mondragon.edu>
2025-10-30 12:04:18 +0100

    Codigo de hundir la flota contra maquina

--- hundirlaflota/src/main/java/com/example/Main.java
+++ hundirlaflota/src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 import javax.swing.SwingUtilities;
 
-public class HundirLaFlota {
+public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new GameFrame().setVisible(true));
     }
